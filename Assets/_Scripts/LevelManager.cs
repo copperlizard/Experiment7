5069383616e7c155e83f17f1bf8e0c0c51ae3520
Cam McKasson <cam@mckassons.com>
2017-02-01 17:11:53 -0800

    finished proto-menus, need to start character controller...

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> m_levelDetails = new List<GameObject>();
+
+    private int m_visibleDetails = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_levelDetails[m_visibleDetails].SetActive(true);		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SetVisibleDetails (int i)
+    {
+        if (i >= m_levelDetails.Count)
+        {
+            m_visibleDetails = 0;
+
+            Debug.Log("i exceeds level index range! i == " + i.ToString());
+        }
+        else
+        {
+            m_levelDetails[m_visibleDetails].SetActive(false);
+            m_visibleDetails = i;
+            m_levelDetails[m_visibleDetails].SetActive(true);
+        }    
+    }
+
+    public void LoadLevel ()
+    {
+        if (m_visibleDetails + 1 >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("no scene[" + (m_visibleDetails + 1).ToString() + "]; check build settings!");
+        }
+        else
+        {
+            SceneManager.LoadScene(m_visibleDetails + 1);
+        }        
+    }
+}
---
+++ Assets/_Scripts/MainMenuPanelManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPanelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainMenuPanel, m_settingsPanel, m_campaignPanel, m_levelDetailsPanel;
+
+    private Animator m_mainMenuAnimator, m_settingsAnimator, m_campaignAnimator, m_levelDetailsAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if (m_mainMenuPanel == null)
+        {
+            Debug.Log("m_mainMenuPanel not assigned!");
+            return;
+        }
+
+        m_mainMenuAnimator = m_mainMenuPanel.GetComponent<Animator>();
+        if (m_mainMenuAnimator == null)
+        {
+            Debug.Log("could not find m_mainMenuAnimator!");
+            return;
+        }
+
+        if (m_settingsPanel == null)
+        {
+            Debug.Log("m_settingsPanel not assigned!");
+            return;
+        }
+
+        m_settingsAnimator = m_settingsPanel.GetComponent<Animator>();
+        if (m_settingsAnimator == null)
+        {
+            Debug.Log("could not find m_settingsAnimator!");
+            return;
+        }
+
+        if (m_campaignPanel == null)
+        {
+            Debug.Log("m_campaignPanel not assigned!");
+            return;
+        }
+
+        m_campaignAnimator = m_campaignPanel.GetComponent<Animator>();
+        if (m_campaignAnimator == null)
+        {
+            Debug.Log("could not find m_campaignAnimator!");
+            return;
+        }
+
+        if (m_levelDetailsPanel == null)
+        {
+            Debug.Log("m_levelDetailsPanel not assigned!");
+            return;
+        }
+
+        m_levelDetailsAnimator = m_levelDetailsPanel.GetComponent<Animator>();
+        if (m_levelDetailsAnimator == null)
+        {
+            Debug.Log("could not find m_levelDetailsAnimator!");
+            return;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OpenMainMenu ()
+    {
+        m_mainMenuAnimator.SetBool("visible", true);
+        m_settingsAnimator.SetBool("visible", false);
+        m_campaignAnimator.SetBool("visible", false);
+        m_levelDetailsAnimator.SetBool("visible", false);
+    }
+
+    public void OpenSettingsMenu()
+    {
+        m_mainMenuAnimator.SetBool("visible", false);
+        m_settingsAnimator.SetBool("visible", true);
+        m_campaignAnimator.SetBool("visible", false);
+        m_levelDetailsAnimator.SetBool("visible", false);
+    }
+
+    public void OpenCampaignMap()
+    {
+        m_mainMenuAnimator.SetBool("visible", false);
+        m_settingsAnimator.SetBool("visible", false);
+        m_campaignAnimator.SetBool("visible", true);
+        m_levelDetailsAnimator.SetBool("visible", false);
+    }
+
+    public void OpenLevelDetails()
+    {
+        m_mainMenuAnimator.SetBool("visible", false);
+        m_settingsAnimator.SetBool("visible", false);
+        m_campaignAnimator.SetBool("visible", true);
+        m_levelDetailsAnimator.SetBool("visible", true);
+    }
+}
